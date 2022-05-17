5f393c41afa2c93fcae5dafbc872642a7efb46a7
sanjay <84283520+lsanjayl@users.noreply.github.com>
2022-05-17 14:36:12 +0530

    pyScript-sanjay

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 import Landing from"./Pages/Landing"
+import Details from"./Pages/Details"
+import Forms from"./Pages/Forms"
+import Suggestion from"./Pages/Suggestion"
+import { Route, Routes } from 'react-router-dom'
 function App() {
   return (
-    <Landing/>
+    <Routes>
+      <Route path="/" element={<Landing/>}></Route>
+      <Route path="/details" element={<Details/>}></Route>
+      <Route path="/forms" element={<Forms/>}></Route>
+      <Route path="/suggestion" element={<Suggestion/>}></Route>
+    </Routes>
   );
 }
 
---
+++ src/services/pyScript.js
@@ -0,0 +1,23 @@
+const  { PythonShell }  = require('python-shell');
+
+export const pyScript=({inputValues})=>{
+           const options = {
+               mode: 'text',
+               //pythonPath: '/usr/bin/python', 
+               pythonOptions: ['-u'],
+               // make sure you use an absolute path for scriptPath
+               //scriptPath: '//Users/91877/Desktop/nodeProjects/live in lab/',
+               args: [inputValues]
+             };
+               PythonShell.run('pythonScript.py', options, function (err, results) {
+                   if (err) throw err;
+                   // results is an array consisting of messages collected during execution
+                   console.log('results: %j', results);
+                   const output = parseInt(results[0])
+                   console.log(output);
+                   return output;                 
+                });
+           
+              
+       
+}
